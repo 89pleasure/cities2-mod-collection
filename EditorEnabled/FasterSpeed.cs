607111cf58a5a044c86819a49aa14b6afa3b6e9b
89pleasure <133201120+89pleasure@users.noreply.github.com>
2023-11-04 19:15:27 +0100

    Added mod EnableUIDebug Some fixes Cleanup

---
+++ EditorEnabled/FasterSpeed.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections.Generic;
+using Game.Simulation;
+using HarmonyLib;
+using System.Reflection.Emit;
+
+namespace EditorEnabled
+{
+    [HarmonyPatch(typeof(SimulationSystem), "OnUpdate")]
+    public class SimulationSystem_OnUpdatePatch
+    {
+        public static void Prefix(IEnumerable<CodeInstruction> instructions)
+        {
+            foreach (var instruction in instructions)
+            {
+                if (instruction.opcode == OpCodes.Call)
+                {
+                    instruction.operand = 1f;
+                }
+            }
+        }
+    }
+}
---
+++ EnableUIDebug/Patches/UIManagerPatches.cs
@@ -0,0 +1,14 @@
+﻿using Colossal.UI;
+using HarmonyLib;
+
+namespace EnableUIDebug.Patches
+{
+    [HarmonyPatch(typeof(UIManager.Settings), "SetDefault")]
+    public class UIManager_SetDefaultPatch
+    {
+        public static void Postfix(UIManager.Settings __instance)
+        {
+            __instance.enableDebugger = true;
+        }
+    }
+}
